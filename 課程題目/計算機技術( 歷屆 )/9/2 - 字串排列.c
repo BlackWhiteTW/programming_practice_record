0a0f14d057bcf2353bca2632eb03d349bebe8b66
blackwhitetw <9131419hihi@gmail.com>
2025-09-27 00:46:02 +0800

    日常更新 - 計算機技術( 歷屆 )  目前進度 #9 迴文明天寫

--- 課程題目/計算機技術( 歷屆 )/9/2 - 字串排列.c
+++ 課程題目/計算機技術( 歷屆 )/9/2 - 字串排列.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 
+int sortsort( char str[] )
+{
+    for ( int i = 0 ; str[i] != '\0' ; i++ )
+        for ( int j = i + 1 ; str[j] != '\0' ; j++ )
+            if ( str[i] > str[j] )
+            {
+                char t = str[i];
+                str[i] = str[j];
+                str[j] = t;
+            }
+}
+
 int main()
 {
     char str[13] = "";
+    int ans = 1;
+    int c = 1;
     printf("Please input a string:");
     scanf("%s", str);
 
-    long long int ans = 1;
-    for (int i = 1; str[i] != '\0'; i++)
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        ans *= i;
+        ans *= i + 1;
     }
-    printf("%lld\n", ans);
 
-    sort(str);
+    sortsort(str);
+
+    for( int i = 0 ; str[i] != '\0' ; i++ )
+    {
+        if ( str[i] == str[i+1] )
+            c++;
+        else
+            c = 1;
+        ans /= c;
+    }
+    printf("%d\n", ans);
 }
---
+++ 課程題目/計算機技術( 歷屆 )/9/3 -迴文.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[101] = "";
+    printf("請輸入一個字串 : ");
+    scanf("%s", str);
+}
