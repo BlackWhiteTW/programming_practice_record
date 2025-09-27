0aaf93de031395419aae4966a699563316461315
blackwhitetw <9131419hihi@gmail.com>
2025-09-27 16:59:24 +0800

    日常更新 - 計算機技術( 歷屆 )  目前進度 #10 指標排序完成

---
+++ 課程題目/計算機技術( 歷屆 )/10/1 - 指標排序.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void sort( int *arr , int n )
+{
+    for ( int i = 0 ; i < n - 1 ; i++ )
+        for ( int j = i + 1 ; j < n ; j++ )
+            if ( *(arr + i) > *(arr + j) )
+            {
+                int t = *(arr + i);
+                *(arr + i) = *(arr + j);
+                *(arr + j) = t;
+            }
+}
+
+int main()
+{
+    int n ;
+    printf("請輸入整數的個數 : ");
+    scanf("%d", &n);
+    int arr[n] ;
+    for ( int i = 0 ; i < n ; i++ )
+    {
+        printf("請輸入第 %d 個數 : ", i + 1 );
+        scanf("%d", &arr[i]);
+    }
+
+    sort( arr, n);
+
+    printf("排序後的結果 : ");
+    for ( int i = 0 ; i < n ; i++ )
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
--- 課程題目/計算機技術( 歷屆 )/9/3 -迴文.c
+++ 課程題目/計算機技術( 歷屆 )/9/3 -迴文.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+
+int is_palindrome ( char str[] , int start , int end )
+{
+    int tmp = 0 , time = 0;
+    if ( start + end % 2 != 0 )
+        tmp = 1;
+    for ( int i = start ; i <= (start + end) / 2 ; i++ )
+    {
+        time++;
+        if ( str[i] != str[end + tmp - time] )
+            return 0;
+    }
+    return 1;
+}
 
 int main()
 {
     char str[101] = "";
     printf("請輸入一個字串 : ");
     scanf("%s", str);
+    int slen = 0 , len[100] = {0} , max_len = 1 , max_len_tmp = 0 , printf_time = 0;
+    slen = strlen(str);
 
-    printf("請輸入一個字串 : ");
+    for ( int i = 0 ; i < slen ; i++ )
+    {
+        for ( int j = i ; j < slen ; j++ )
+        {
+            if ( is_palindrome( str , i , j ) )
+            {
+                len[i] = j;
+                if ( j - i + 1 > max_len )
+                {
+                    max_len_tmp = 0;
+                    max_len = j - i + 1;
+                }
+                if ( j - i + 1 == max_len )
+                    max_len_tmp++;
+            }
+        }
+    }
+
+    printf("最長迴文為 : ");
+    for ( int i = 0 ; i < slen ; i++ )
+    {
+        if ( len[i] - i + 1 == max_len )
+        {
+            for ( int j = i ; j <= len[i] ; j++ )
+                printf("%c", str[j]);
+            if ( printf_time < max_len_tmp - 1)
+                printf(", ");
+            printf_time++;
+        }
+    }
 }
